93b197f3b5844b2e5c088465364db85658abca23
rnegi0598 <rnegi0598@gmail.com>
2023-08-26 00:42:44 +0530

    added mongo atlas

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dbConnect = async () => {
   try {
     await mongoose.connect(
-      "mongodb://localhost:27017",
+      "mongodb+srv://rnegi0598:hNSc9UrmcQ73MeJT@cluster0.j0elyxo.mongodb.net/?retryWrites=true&w=majority",
       {
         dbName: "polling-api",
       }
